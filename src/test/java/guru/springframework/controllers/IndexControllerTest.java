864cae519589ca338b86d26cce7a50c40b8936f8
mkyagulanyi <mkyagulanyi@apple.com>
2022-07-23 12:38:57 -0500

    Adds IndexController and RecipeServiceImpl tests

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,62 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class IndexControllerTest {
+    // Setup
+    private IndexController indexController;
+    @Mock
+    private RecipeService recipeService;
+
+    @Mock
+    private Model model;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() throws Exception {
+        // Given: Setup expectations
+        Set<Recipe> expectedData = new HashSet<>();
+        Recipe recipeOne = new Recipe();
+        Recipe recipeTwo = new Recipe();
+        recipeTwo.setId(1L);
+        expectedData.add(recipeOne);
+        expectedData.add(recipeTwo);
+        when(recipeService.getRecipes()).thenReturn(expectedData);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        // When: Actual executions of the action/method
+        String viewName = indexController.getIndexPage(model);
+
+        // Then: Assert and verify
+        assertEquals("index", viewName);
+        // Verify interactions between mocks and controller
+        // Verify that mocks methods are really being executed.
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
+    }
+}
---
+++ src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -0,0 +1,44 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+    private RecipeServiceImpl recipeService;
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+        Recipe recipe = new Recipe();
+        Set<Recipe> expectedRecipes = new HashSet<>();
+        expectedRecipes.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(expectedRecipes);
+
+        Set<Recipe> actualRecipes = recipeService.getRecipes();
+
+        assertEquals(actualRecipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+
+}
